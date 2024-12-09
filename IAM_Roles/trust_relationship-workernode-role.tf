303e96f9ddbdc5148ea30c4ccb53b213b74dff6d
Mevalal Saroj <sarojmeva@gmail.com>
2024-12-09 16:02:48 +0530

    Update trust_relationship-workernode-role.tf

--- IAM_Roles/trust_relationship-workernode-role.tf
+++ IAM_Roles/trust_relationship-workernode-role.tf
@@ -5,8 +5,26 @@
             "Effect": "Allow",
             "Principal": {
                 "Service": "ec2.amazonaws.com"
+                 "AWS": "<WorkerNode_Role_ARN>"
             },
             "Action": "sts:AssumeRole"
         }
     ]
 }
+===============================================================================
+Example:
+--------------
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "ec2.amazonaws.com"
+                 "AWS": "arn:aws:iam::xxxxxxx:role/eks-workernode-role"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}
+===============================================================================
