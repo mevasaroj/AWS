705d4356698d01779dca06d619d7548bef8a3b79
Mevalal Saroj <sarojmeva@gmail.com>
2024-12-09 16:08:36 +0530

    Update 04_02_trust-relationship-terraform-role.tf

--- IAM_Roles/04_02_trust-relationship-terraform-role.tf
+++ IAM_Roles/04_02_trust-relationship-terraform-role.tf
@@ -1,16 +1,29 @@
 {
-  "Version": "2012-10-17",
-  "Statement": [
-     {
-       "Effect": "Allow",
-       "Principal": {
-         "AWS": [
-           "<Terraform_role_ARN>"
-          ],
-         "Service": "ec2.amazonaws.com"
-       },
-       "Action": "sts:AssumeRole",
-       "Condition": {}
-     }
-  ]
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "ec2.amazonaws.com"
+                 "AWS": "<Terraform_role_ARN>"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}
+===============================================================================
+Example:
+--------------
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "ec2.amazonaws.com"
+                 "AWS": "arn:aws:iam::xxxxxxx:role/terafform-role"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
 }
